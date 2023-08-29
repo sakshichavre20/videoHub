aa5356b049c3b1cccc183e04f46c1bdd43f83767
sakshichavre20 <chavresakshi@gmail.com>
2023-08-30 00:20:50 +0530

    initial setup chakaraUI

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import React from "react";
+import { IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { FaMoon, FaSun } from "react-icons/fa";
 
-export const ColorModeSwitcher = props => {
+const ColorModeSwitcher = props => {
   const { toggleColorMode } = useColorMode();
   const text = useColorModeValue('dark', 'light');
   const SwitchIcon = useColorModeValue(FaMoon, FaSun);
 
   return (
     <IconButton
-      size="md"
-      fontSize="lg"
-      aria-label={`Switch to ${text} mode`}
       variant="ghost"
+      position="fixed"
+      top="4"
+      right="4"
       color="current"
       marginLeft="2"
       onClick={toggleColorMode}
@@ -21,3 +21,5 @@ export const ColorModeSwitcher = props => {
     />
   );
 };
+
+export default ColorModeSwitcher;
---
+++ src/components/Header.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { BiMenuAltLeft } from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  VStack,
+  HStack,
+} from '@chakra-ui/react';
+
+const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button
+        position="fixed"
+        top="4"
+        left="4"
+        colorScheme="purple"
+        p="0"
+        width={'10'}
+        h="10"
+        borderRadius="full"
+        onClick={onOpen}
+      >
+        <BiMenuAltLeft size={'20'} />
+      </Button>
+
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>VIDEO HUB</DrawerHeader>
+          <DrawerBody>
+            <VStack alignItems="flex-start">
+              <Button onClick={onClose} colorScheme="purple" variant="ghost">
+                <Link to="/">Home</Link>
+              </Button>
+              <Button onClick={onClose} colorScheme="purple" variant="ghost">
+                <Link to="/videos">Videos</Link>
+              </Button>
+              <Button onClick={onClose} colorScheme="purple" variant="ghost">
+                <Link to="/videos?category=free">Free Videos</Link>
+              </Button>
+              <Button onClick={onClose} colorScheme="purple" variant="ghost">
+                <Link to="/upload">Upload Video</Link>
+              </Button>
+            </VStack>
+          </DrawerBody>
+          <DrawerFooter>
+            <HStack w="full" justifyContent="space-evenly">
+              <Button onClick={onClose} colorScheme="purple">
+                <Link to="/login">Log In</Link>
+              </Button>
+              <Button
+                onClick={onClose}
+                colorScheme="purple"
+                variant={'outline'}
+              >
+                <Link to="/signup">Sign up</Link>
+              </Button>
+            </HStack>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default Header;
