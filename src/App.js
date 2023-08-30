435fa5eff3bcf429dc3b47e4e17eabf803446ee5
sakshichavre20 <chavresakshi@gmail.com>
2023-08-30 22:47:04 +0530

    [ADD] Footer

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route />
       </Routes>
+      <Footer />
     </Router>
   );
 }
